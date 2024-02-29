5bf11bb016467eecc6490cbd37191c06a97294ce
Aakash Kashyap <141166752+aak1675786769809809@users.noreply.github.com>
2024-02-29 13:17:46 +0530

    Add files via upload

---
+++ aak111.py
@@ -0,0 +1,11 @@
+# Python code for "HEY AAKASH" with details
+
+def print_message():
+    """
+    This function prints the message "HEY AAKASH".
+    """
+    message = "HEY AAKASH"
+    print(message)
+
+# Calling the function to print the message
+print_message()
